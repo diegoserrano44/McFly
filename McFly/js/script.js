ff0bf8547f17480d06f92497eee614905a7a60cc
diegoserrano44 <71322694+diegoserrano44@users.noreply.github.com>
2021-12-02 18:03:59 +0100

    Add files via upload

---
+++ McFly/js/script.js
@@ -0,0 +1,29 @@
+//Dropdown Login Menu
+$(document).ready(function() {
+	$('#showlogin').click(function() {
+	  $('#loginpanel').slideToggle();
+	});
+ });
+
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+    navbar.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
